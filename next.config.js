bc06788b2550e72a1a44e0feb804e699380ba500
lialila <uche0790@gmail.com>
2023-05-12 15:17:36 +0200

    Add sass to package.json

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    typedRoutes: true,
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
